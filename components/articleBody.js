ab83577b8d01c2204a5c6964cdf656352f76a34b
Israel-oduguwa <adeboyega22@gmail.com>
2021-01-16 13:03:04 +0100

    set the Article Body

--- components/articleBody.js
+++ components/articleBody.js
@@ -3,27 +3,33 @@ import Interweave from "interweave";
 import Link from "next/link";
 import Typography from '@material-ui/core/Typography';
 import Avatar from '@material-ui/core/Avatar';
+import dayjs from "dayjs";
+import LocalizedFormat from "dayjs/plugin/LocalizedFormat";
 export class articleBody extends Component {
     render() {
-        const {blog, blogUtils} = this.props
+        const {blog, blogUtils} = this.props;
+        dayjs.extend(LocalizedFormat)
         return (
            <>
-           <div className="container">
-               <div className="col-md-8">
+           <div className="container top">
+               <div className="col-md-9">
                    <div className="article-info">
                    <div className="the-title">
                         <Typography variant="h4" gutterBottom>
                           {blog.title}
                         </Typography>
                     </div>
+                    
                     <div className="km">
-                     <Link href={`/user/${blog.userId}/${blog.fullName.replace(/\s+/g, '-')}`}>
+                     <Link href={`/user/${blog.userId}/${blog.fullName.replace(/\s+/g, '-')}`} >
                          <a>
                      <div className="ar">
-                  <Avatar className="blogPageAvatar" alt={blog.fullName} src={blog.AuthorImage} />
+                  <Avatar alt={blog.fullName} src={blog.AuthorImage} />
                          <p class="card-text"><small class="text-muted ro">{blog.fullName}</small></p>
+                         <p class="card-text"><small class="text-muted">{dayjs(blog.dataCreated).format('ll')}</small></p>
                    </div>
-                              </a>   </Link>
+                          </a> 
+                            </Link>
                        </div>                   
                    </div>
                    <div className="article-body">
