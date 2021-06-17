d465f7c8c5c2fa128bb1b5c5236478c88a27744c
Israel-oduguwa <adeboyega22@gmail.com>
2021-06-17 21:41:03 +0100

    Landingf page sections

--- components/HomePage/JobFeautures.js
+++ components/HomePage/JobFeautures.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
 import { withStyles } from '@material-ui/core/styles';
-
+import Button from '@material-ui/core/Button';
+import TrendingFlatIcon from '@material-ui/icons/TrendingFlatIcon';
+import SearchJob from "../SearchJob";
+import Icon from '@material-ui/core/Icon';
 const styles = (theme) =>({
 
 })
@@ -27,7 +30,11 @@ export class JobFeautures extends React.Component {
 						<div className="col-md-5 mt-4">
                       		<Typography className="topPhrase mb-3 mt-4" variant="h4">Browse Jobs Faster</Typography>
                       		<Typography variant="body1">allIt’s fast, it’s easy, and it can make a professional, full-page resume for anyone. Regardless of how much work experience you have, how long you went to school, or what skills you possess, our software was designed by certified resume writers to generate a complete resume for every kind of job seeker. div</Typography>
-                      		<a href="#">Browse Jobs ==></a>
+                      			<Button
+						        color="primary"
+						        endIcon={<Icon>TrendingFlatIcon</Icon>}
+					     	 	>
+					     	 	Check Jobs </Button>
                    		</div>  
                    		<div className="col-md-7 mt-4 mb-4">
                     		<img src="https://evernote.com/c/assets/homepage/feature_document_scanning__en.png?82b87618cf404580" alt="..."/>
@@ -36,9 +43,16 @@ export class JobFeautures extends React.Component {
 	                    	<img src="https://clickup.com/landing/images/v2/switch-to-the-best-project-management.png" alt="..."/>
 	                    </div>
 	                     <div className="col-md-5 offset-md-1 mt-2">
-	                    	<Typography className="topPhrase mb-3 mt-2" variant="h4">Acess to knowledge to advance your career</Typography>
+	                    	<Typography className="topPhrase mb-3 mt-2" variant="h4">Lightining Fast Search with Filters for Easy Job Search</Typography>
 	                    	<Typography variant="body1">allIt’s fast, it’s easy, and it can make a professional, full-page resume for anyone. Regardless of how much work experience you have, how long you went to school, or what skills you possess, our software was designed by certified resume writers to generate a complete resume for every kind of job seeker. div</Typography>
-	                    	<a href="#">Check Tips knowledge ==></a>
+	                    	<SearchJob/>
+	                    	<Button
+						        
+						        color="primary"
+						        
+						        endIcon={<Icon>TrendingFlatIcon</Icon>}
+					     	 	>
+					     	 	Enter Jobs and Search</Button>
 	                    </div>  
 	                    <div className="col-md-5 d-lg-none mt-4 mb-4">
 	                    	<img src="https://clickup.com/landing/images/v2/switch-to-the-best-project-management.png" alt="..."/>
