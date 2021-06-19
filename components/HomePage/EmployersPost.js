64cb7b863ec9a0d36bf7c6d8d0e3bc3db4e097e0
Israel-oduguwa <adeboyega22@gmail.com>
2021-06-19 13:05:34 +0100

    Landingf page sections

--- components/HomePage/EmployersPost.js
+++ components/HomePage/EmployersPost.js
@@ -4,21 +4,23 @@ import Button from '@material-ui/core/Button';
 export class EmployersPost extends React.Component {
 	render() {
 		return (
-			<div className="container">
-				<div className="LPMT">
-					<div className="row">
-						<div className="col-md-12 text-center">
-							<Typography variant="h4" className="FH LPTC">
-			                    For Employers Looking to Post a Job
-			                 </Typography>
-			                 <Typography variant="h6" className="STH">
-			                     Post and advertize your company Job Information on our platform
-			                 </Typography>
-			                 <div className="mt-4">
-			                    <Button variant="contained" size ="large" style={{boxShadow: "rgb(178 186 230) 0px 10px 25px", borderRadius:"5px",boxShadow: "rgb(178 186 230) 0px 10px 25px"}} color="primary">
-			                      Post a Job
-			                    </Button>
-			                 </div>
+			<div className="LPMT">
+				<div className="container">
+					<div className="LPMT">
+						<div className="row">
+							<div className="col-md-12 text-center">
+								<Typography variant="h4" className="FH LPTC">
+				                    For Employers Looking to Post a Job
+				                 </Typography>
+				                 <Typography variant="h6" className="STH">
+				                     Post and advertize your company Job Information on our platform
+				                 </Typography>
+				                 <div className="mt-4">
+				                    <Button variant="contained" size ="large" style={{boxShadow: "rgb(178 186 230) 0px 10px 25px", borderRadius:"5px",boxShadow: "rgb(178 186 230) 0px 10px 25px"}} color="primary">
+				                      Post a Job
+				                    </Button>
+				                 </div>
+							</div>
 						</div>
 					</div>
 				</div>
